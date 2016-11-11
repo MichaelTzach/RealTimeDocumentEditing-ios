2af5d36952ade9541a5fa871e31def77239747f4
I <michael.tzach@jivesoftware.com>
2016-11-11 10:17:36 +0200

    Initial commit

---
+++ RealTimeDocumentEditing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealTimeDocumentEditing
+//
+//  Created by Michael Tzach on 11/11/16.
+//  Copyright © 2016 Michael Tzach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
